eb54c3caf4659c88d269e9f1dc41e2bab6f8fa8e
Rachel <rachelkithae@yahoo.com>
2023-03-19 07:46:12 -0700

    rev

--- 0x00-hello_world/print.c
+++ 0x00-hello_world/print.c
@@ -1,8 +1,8 @@
-/*print function*/
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
