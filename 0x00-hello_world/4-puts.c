8c25c453854061c90440540d45d4189564e38da3
Rachel <rachelkithae@yahoo.com>
2023-03-19 07:24:35 -0700

    rev

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 /*prints a statement*/
 #include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+
+ int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
