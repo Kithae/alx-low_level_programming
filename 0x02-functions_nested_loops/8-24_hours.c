97f5538e5944ae9f87b23f39998e04fe012301e6
Rachel <rachelkithae@yahoo.com>
2023-03-21 12:16:46 -0700

    re

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* jack_b - function that prints every minute of the day
+* jack_bauer - function that prints every minute of the day
 * starting from 00:00 to 23:59
 */
 void jack_bauer(void)
