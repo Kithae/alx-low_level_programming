dc518f126e8184045c3c5a3b72b84431d65d4219
Rachel <rachelkithae@yahoo.com>
2023-03-23 04:11:29 -0700

    rev

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul - a function that multiplies two integers.
+ *@a: first integer
+ *@b: second integer
+ *
+ * Return: the results of the multiplication.
+ */
+
+int mul(int a, int b)
+{
+
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - a function that prints the numbers from 0 to 9.
+ *
+ * Return: 0 success.
+ */
+
+void print_numbers(void)
+{
+char i;
+for (i = 'o'; i <= '9'; i++)
+{
+putchar (i);
+}
+putchar('\n');
+
+}
