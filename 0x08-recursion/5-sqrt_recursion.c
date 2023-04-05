3e9b731b79114672dc081d7a4de511353aee642f
Rachel <rachelkithae@yahoo.com>
2023-04-05 14:54:47 -0700

    rev3

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,22 +2,6 @@
 #include <stdio.h>
 
 /**
- * sqrt_2 – checks between 1 to n.
- * @x: the integer similar to n
-* @y: the integer that checks from 1 to n
-* Return: 0, success, else -1 error
- */
-
-int sqrt_2(int x, int y)
-{
-	if (y * y == x)
-		return (y);
-	else if (y * y > x)
-		return (-1);
-	return (sqrt_2(x, y + 1));
-}
-
-/**
  * _sqrt_recursion - a function that returns the natural square root.
  * @n: the integer
 * Return: -1 error, else the result
@@ -25,5 +9,22 @@ int sqrt_2(int x, int y)
 
 int _sqrt_recursion(int n)
 {
-return (sqrt2(n, 1));
+return (sqrt_2(n, 1));
+}
+
+/**
+ * sqrt_2 – checks between 1 to n.
+ * @x: the integer similar to n
+* @y: the integer that checks from 1 to n
+* Return: value if success, else -1 error
+ */
+
+int sqrt_2(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y < x)
+		return (sqrt_2(x, y + 1));
+	else
+		return (-1);
 }
