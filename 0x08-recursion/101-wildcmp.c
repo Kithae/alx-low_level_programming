b887fbdae8c71d2bf66e98862d488daeac6b56d0
Rachel <rachelkithae@yahoo.com>
2023-04-05 14:08:07 -0700

    rev

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,9 +8,9 @@
 * Return: 1 if identical, else 0
  */
 
- int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
 	if (*s1 == *s2)
