939a58ef023f1da30246e6c9468488782023e2d0
Rachel <rachelkithae@yahoo.com>
2023-04-17 03:38:57 -0700

    first read me

---
+++ 0x0E-structures_typedef/Dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog – the dog structure
+ * @name: a character pointer to a string
+ * @age: float data type
+ * @owner: a character pointer to a string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+‘This directory contains practice files on C - Structures, typedef done during software engineering program at ALX’
