ad6b6735136642a8bafec71de5ee002a298993cc
Rachel <rachelkithae@yahoo.com>
2023-04-17 03:45:30 -0700

    rev

--- 0x0E-structures_typedef/Dog.h
+++ 0x0E-structures_typedef/Dog.h
@@ -1,22 +1,31 @@
 #ifndef DOG_H
 #define DOG_H
-/**
+
+	/**
  * struct dog – the dog structure
  * @name: a character pointer to a string
  * @age: float data type
  * @owner: a character pointer to a string
+ * Description: Longer description
  */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
 
+struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+	/**
+	 * dog_t - typedef for the dog structure
+	 */
+	typedef struct dog dog_t;
 
-#endif
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+	#endif
