2b014fa228c59e403c9182336a9db4adf7b69cea
Rachel <rachelkithae@yahoo.com>
2023-04-30 00:19:46 -0700

    rev

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,6 +35,7 @@ struct listp_s *next;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+void free_listp2(listp_t **head);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
