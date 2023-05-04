5637bcc0fb5497946a53c6a72a410084de73ac12
Rachel <rachelkithae@yahoo.com>
2023-05-04 04:58:12 -0700

    rev

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,22 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-int a = 0
-	unsigned int bin_num = 0
+int a;
+unsigned int binNum;
 
-		if (b == 0)
-		return (0);
-	for (b[a] != '\0'; a++)
-	{
-		if (b[a] != '0' && b[a] != '1')
-			return (0);
-	}
-	for (b[a] != '\0'; a++)
-	{
-		bin_num <<= 1;
-		if (b[a] == '1')
-			bin_num += 1;
-	}
-	return (bin_num);
+binNum = 0;
+if (b == 0)
+return (0);
+for (a = 0; b[a] != '\0'; a++)
+{
+if (b[a] != '0' && b[a] != '1')
+return (0);
+}
+for (a = 0; b[a] != '\0'; a++)
+{
+binNum <<= 1;
+if (b[a] == '1')
+binNum += 1;
+}
+return (binNum);
 }
